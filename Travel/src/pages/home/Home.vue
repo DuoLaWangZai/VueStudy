c6909692f0e12d6fc4e8e5679230c08c25e3834a
wangshupeng <404251831@qq.com>
2018-06-10 14:20:15 +0800

    Travel project init

---
+++ Travel/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+
+	<div>
+	  <h1>home</h1>
+	  <router-link to="/list">Links</router-link>
+	</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+
+</style>
